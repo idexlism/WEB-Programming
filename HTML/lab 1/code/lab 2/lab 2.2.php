49fe27163c6a42b3741e5d3bcc5939d5099f6ae9
Elbeg <dlyaorigina2002@mail.ru>
2024-03-20 21:07:41 +0200

    create task 2

---
+++ HTML/lab 1/code/lab 2/lab 2.2.php
@@ -0,0 +1,16 @@
+<?php
+$int = 31;
+
+echo $int;
+
+echo "\n";
+
+$pi = 3.14;
+echo "$pi\n";
+
+echo 22 - 10 . "\n";
+#-----
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+
+echo abs($lastMonth - $thisMonth) . "\n";
