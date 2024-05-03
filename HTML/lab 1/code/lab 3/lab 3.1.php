933e155759264dee2cc23e10789ac00c583be077
Elbeg <dlyaorigina2002@mail.ru>
2024-05-03 16:05:42 +0200

    task 3.1(2) done

--- HTML/lab 1/code/lab 3/lab 3.1.php
+++ HTML/lab 1/code/lab 3/lab 3.1.php
@@ -6,3 +6,9 @@ $match = array(); //пустой массив
 $count = preg_match_all($find, $str, $match);
 echo "Найдено строк: {$count}\n";
 
+//2. Дана строка с целыми числами 'a1b2c3'. С помощью регулярки преобразуйте строку так, чтобы вместо этих чисел стояли их кубы.
+$numb = 'a1b2c3';
+$changeNumb = '![0-9]+!';
+$degree = function ($matches){return pow($matches[0], 3);};
+$result = preg_replace_callback($changeNumb,$degree,$numb);
+echo $result;
