185d9d0be48122d93edcaeaf8e36df9201686351
Elbeg <dlyaorigina2002@mail.ru>
2024-05-03 16:08:59 +0200

    task 3.2a done

---
+++ HTML/lab 1/code/lab 3/lab 3.2/task2.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Task 2a.</h1>
+        <form action="text_parsing.php" method="post">
+            <label for="text-example">Введите текст</label>
+            <br>
+            <textarea name="text-example" cols="50" rows="5"></textarea>
+            <br>
+            <input type="submit" value="Отправить">
+        </form>
+    </div>
+
