f8a6aae11f033a7b568d250f9ec46b8214564883
Elbeg <dlyaorigina2002@mail.ru>
2024-06-04 20:57:57 +0200

    implement SQL queries

--- HTML/lab 1/code/lab 5/mysql.php
+++ HTML/lab 1/code/lab 5/mysql.php
@@ -3,4 +3,14 @@ if(mysqli_connect_errno())
 {
     printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
     exit;
-}
+}$mysqli->query('INSERT INTO ad(email, title, description, category) VALUES("test@test.com", "title", "desc", "other")');
+ if($result = $mysqli->query('SELECT * FROM ad ORDER BY created DESC'))
+ {
+     print("Ads:\n");
+     while($row = $result->fetch_assoc())
+     {
+         printf("%s(%s)\n",$row['email'], $row['title']);
+     }
+     $result->close();
+ }
+ $mysqli->close();
