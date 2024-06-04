402af828cdb678de6c1a0ed699d1523ef098ad42
Elbeg <dlyaorigina2002@mail.ru>
2024-06-04 20:58:12 +0200

    Update mysql.php

--- HTML/lab 1/code/lab 5/mysql.php
+++ HTML/lab 1/code/lab 5/mysql.php
@@ -4,6 +4,7 @@ if(mysqli_connect_errno())
     printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
     exit;
 }$mysqli->query('INSERT INTO ad(email, title, description, category) VALUES("test@test.com", "title", "desc", "other")');
+$mysqli->query('INSERT INTO ad(email, title, description, category) VALUES("test@test.com", "title", "desc", "other")');
  if($result = $mysqli->query('SELECT * FROM ad ORDER BY created DESC'))
  {
      print("Ads:\n");
